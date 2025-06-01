11046eb90e05649cf13519a941225e3fa46b9c9d
Keerthes <100361099+Terminator-Soul@users.noreply.github.com>
2025-06-01 19:54:39 +0530

    Add files via upload

---
+++ Cut_Ribbon.py
@@ -0,0 +1,28 @@
+n, a, b, c = map(int, input().split())
+
+
+def retsum(x, y):
+    r = n - (x * a + y * b)
+    return r
+
+
+def process():
+    x = 0
+    m = 0
+    while x < (n / a) + 1:
+        y = 0
+        while y < (n / b) + 1:
+            s = retsum(x, y)
+            # print s
+            if s >= 0 and s % c == 0:
+                z = s / c
+                m = max(m, x + y + z)
+            y += 1
+        x += 1
+    print(int(m))
+
+
+if n % min(a, b, c) == 0:
+    print(int(n / min(a, b, c)))
+else:
+    process()
