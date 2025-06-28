50c2e7a5283c749af4645c7b15f8f5f3319e301b
Keerthes <100361099+Terminator-Soul@users.noreply.github.com>
2025-06-28 23:04:02 +0530

    Add files via upload

--- Football.py
+++ Football.py
@@ -1,7 +1,5 @@
-q = "0000000"
-p = "1111111"
-k = input()
-if q in k or p in k:
-    print("YES")
-else:
-    print("NO")
+from collections import Counter
+
+n = int(input())
+goals = Counter(input().strip() for _ in range(n))
+print(goals.most_common(1)[0][0])
