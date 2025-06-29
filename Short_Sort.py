301da8d6f7649574e82942c3ff858bc9f30b4a20
Keerthes <100361099+Terminator-Soul@users.noreply.github.com>
2025-06-29 16:44:37 +0530

    Add files via upload

---
+++ Short_Sort.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    s = input()
+    count = 0
+    if s.index("a") == 0:
+        count += 1
+    if s.index("b") == 1:
+        count += 1
+    if s.index("c") == 2:
+        count += 1
+
+    if count >= 1:
+        print("YES")
+    else:
+        print("NO")
